35925da57518e84292d3d106b5dbb01ead4776b9
cjdaboss009 <67497735+cjdaboss009@users.noreply.github.com>
2020-09-16 23:47:22 +0000

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,23 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('token');
+client.on('message', message => {
+	console.log(message.content);
+});
+client.on('message', async (message) => {
+    if(message.content === '-suggest') {
+ const exampleEmbed = new Discord.MessageEmbed()
+	.setTitle('***Suggestion Created***')
+		.setDescription('Some description here')
+          .setAuthor('${message.author.username}',${author.avatar}')
+const channel = <client>.channels.cache.get('<id>');
+channel.send(Embed);
+await msg.react("✅")
+await msg.react("❎")
+    }
+});
