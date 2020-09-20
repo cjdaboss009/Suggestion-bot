36100141a1fdb5cd4b1daa43dce53cc0ce96186f
cjdaboss009 <67497735+cjdaboss009@users.noreply.github.com>
2020-09-20 04:50:17 +0000

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,22 +1,18 @@
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.once('ready', () => {
-    console.log('Ready!');
-});
-
-client.login('token');
-client.on('message', message => {
-    console.log(message.content);
-});
-client.on('message', async (message) => {
-    if(message.content === '-suggest') {
- const exampleEmbed = new Discord.MessageEmbed()
-    .setTitle('***Suggestion by ${message.author.username}***')
-        .setDescription('    ${message.author.avatar}')
+const Discord = require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot, message, args, level) => {
+    let x = args.slice(1).join(" ")
+const exampleEmbed = new Discord.MessageEmbed()
+    .setTitle(`***Suggestion by ${message.author.username}***`)
+    .setDescription(`${x}`)
+    .setImage(`${message.author.displayAvatarURL()}`)
 const channel = message.guild.channels.cache.get('id');
-channel.send(exampleEmbed);
-await message.react("✅")
-await message.react("❎")
-    }
-});
+const OwO = await channel.send(exampleEmbed)
+await OwO.react("✅")
+await OwO.react("❎")
+}
+module.exports.help = {
+    name: "suggest",
+    aliases: [],
+    category: "Misc"
+  }
